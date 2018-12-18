a488380e709d8165247dcc47eeed999e4a8e53ad
Matt Edwards <matt.edwards@domo.com>
2018-12-17 17:07:59 -0700

    finish 4 b

--- 4/b/solution.js
+++ 4/b/solution.js
@@ -2,7 +2,7 @@ const readAsync = require('util').promisify(require('fs').readFile);
 const path = require('path');
 
 async function getInput() {
-  const input = await readAsync(path.resolve(__dirname, 'inputa'), 'utf8');
+  const input = await readAsync(path.resolve(__dirname, 'input'), 'utf8');
   return input.split('\n');
 }
 
@@ -12,11 +12,11 @@ async function getInput() {
 //   }
 // }
 
-function sortInput(a,b){
+function sortInput(a, b) {
   const dateRegex = /\[(.*)\]/
-  let [,aDate] = dateRegex.exec(a);
+  let [, aDate] = dateRegex.exec(a);
   aDate = Date.parse(aDate);
-  let [,bDate] = dateRegex.exec(b);
+  let [, bDate] = dateRegex.exec(b);
   bDate = Date.parse(bDate);
   return aDate - bDate;
 }
@@ -33,7 +33,7 @@ getInput().then(input => {
   for (let i = 0; i < input.length; i++) {
     const line = input[i];
     const [, date] = (dateRegex).exec(line);
-    if(idRegex.test(line)){
+    if (idRegex.test(line)) {
       lastId = idRegex.exec(line)[1];
       start = 0;
       continue;
@@ -41,12 +41,15 @@ getInput().then(input => {
 
     const awake = line.toLowerCase().indexOf('wakes') !== -1;
     if (!schedule[lastId]) {
-      schedule[lastId] = {totalAsleep:0,id:lastId}
+      schedule[lastId] = {
+        totalAsleep: 0,
+        id: lastId
+      }
     }
     const [, day, time] = (/(.*)\s\d{2}:(\d+)/).exec(date);
     schedule[lastId][day] = schedule[lastId][day] || new Array(60).fill(0);
-    for(let k = start; k< time;k++){
-      const val = awake?1:0;
+    for (let k = start; k < time; k++) {
+      const val = awake ? 1 : 0;
       schedule[lastId][day][k] = val;
       schedule[lastId].totalAsleep += val;
     }
@@ -55,21 +58,24 @@ getInput().then(input => {
   console.log(getMostAsleepGuard());
 });
 
-function getMostAsleepGuard(){
+function getMostAsleepGuard() {
   let mostAsleepMinute = 0;
-  const guard = Object.keys(schedule).map(key=>schedule[key]).reduce((a,b)=>{
-    return a.totalAsleep > b.totalAsleep ? a: b;
-  });
+  const guards = Object.keys(schedule).map(key => schedule[key]).map(a => ({ ...a,
+    time: Object.keys(a).map(key => a[key]).filter(a => Array.isArray(a))
+  }));
   let minute = 0;
-  const timeMatrix = Object.keys(guard).map(key=>guard[key]).filter(a=>Array.isArray(a));
-  //transpose matrix
-  const transposed = timeMatrix[0].map((col,i,arr)=>timeMatrix.map(row=>row[i]));
+  let chosenGuard;
   let max = 0;
-  transposed.map(time=>time.reduce((a,b)=>a+b)).forEach((a,i)=>{
-    if(a>max){
-      max = a;
-      mostAsleepMinute = i;
-    }
+  //transpose matrix
+  guards.forEach(guard => {
+    const transposed = guard.time[0].map((col, i, arr) => guard.time.map(row => row[i])).map(time => time.reduce((a, b) => a + b));
+    transposed.forEach((a,i)=>{
+      if(a>max){
+        max = a;
+        minute = i;
+        chosenGuard = guard.id;
+      }
+    })
   });
-  return +guard.id*mostAsleepMinute;
+  return +chosenGuard*minute;
 }
