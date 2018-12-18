43dbea7de8668b6fb731ad9f046ac9fccf96e0ff
Matt Edwards <edwardsjm01@gmail.com>
2018-12-17 22:45:15 -0700

    finish day 5

--- 5/a/solution.js
+++ 5/a/solution.js
@@ -3,9 +3,21 @@ const path = require('path');
 
 async function getInput() {
   const input = await readAsync(path.resolve(__dirname, 'input'), 'utf8');
-  return input.split('\n');
+  return input;
 }
 
 getInput().then(input => {
   console.log(input);
+  let edit = true;
+  const ans = input.split('').reduce((a, b) => {
+    let retVal;
+    const diff = Math.abs(a.charCodeAt(a.length - 1) - b.charCodeAt(0));
+    if (diff===32) {
+      retVal = a.slice(0, -1);
+    } else {
+      retVal = a + b;
+    }
+    return retVal;
+  });
+  console.log(ans, ans.length);
 });
--- 5/b/solution.js
+++ 5/b/solution.js
@@ -3,9 +3,28 @@ const path = require('path');
 
 async function getInput() {
   const input = await readAsync(path.resolve(__dirname, 'input'), 'utf8');
-  return input.split('\n');
+  return input;
 }
 
 getInput().then(input => {
-  console.log(input);
+  let min = Infinity;
+  for(let i = 0; i<26;i++){
+    const regex = new RegExp(String.fromCharCode(65+i),'ig');
+    min = Math.min(min,shrink(input.replace(regex,'')));
+  }
+  console.log(min);
 });
+
+function shrink(polymer){
+  const ans = polymer.split('').reduce((a, b) => {
+    let retVal;
+    const diff = Math.abs(a.charCodeAt(a.length - 1) - b.charCodeAt(0));
+    if (diff===32) {
+      retVal = a.slice(0, -1);
+    } else {
+      retVal = a + b;
+    }
+    return retVal;
+  });
+  return ans.length;
+}
